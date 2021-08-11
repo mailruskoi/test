0dc8bf05815526e18cd1bd1ce7c00bae8a5293f7
mailruskoi <53603858+mailruskoi@users.noreply.github.com>
2021-08-11 10:05:50 +0300

    Add files via upload

---
+++ mine_eth.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Change the following address to your ETH addr.
+ADDRESS=0x0415991022668f3DD764896f23D3a11c806f9ADd
+
+USERNAME=$ADDRESS.google1
+POOL=eth-us.sparkpool.com:13333
+# Change SCHEME according to your POOL. For example:
+# ethash:     Nanopool
+# ethproxy:   BTC.com, Ethermine, PandaMiner, Sparkpool
+# ethstratum: Antpool.com, BTC.com, F2pool, Huobipool.com, Miningpoolhub
+SCHEME=ethstratum
+
+./bminer -uri $SCHEME://$USERNAME@$POOL -api 127.0.0.1:1880
